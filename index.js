bc4bc242a9f414b983d92d8b9bdb6ebc8bc6e447
Al <awessonn@gmail.com>
2023-04-03 19:00:45 -0700

    added to questions array.

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'Table of Contents',
+        message: 'Table of Contents',
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Give a description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'How will your project be used?',
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'What steps are required for installing your project. (N/A if no installation is required.)',
+    },
+    {
+        type: 'input',
+        name: 'Contributers',
+        message: 'List any contributers to the project using GitHub user names.',
+    },
+    {
+        type: 'list',
+        name: 'License',
+        message: 'What license does your project have?',
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync()
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
