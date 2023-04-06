223cd4015eaf8ce40e58689f17fcb6ed3d02327b
Al <awessonn@gmail.com>
2023-04-06 15:43:15 -0700

    added functions that returns the license link and returns the license section of the README.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
+
+
 
 
 const questions = [
@@ -8,48 +11,58 @@ const questions = [
         type: 'input',
         name: 'Title',
         message: 'What is the title of your project?',
-    },
-    {
-        type: 'input',
-        name: 'Table of Contents',
-        message: 'Table of Contents',
+        default: "MyProject",
     },
     {
         type: 'input',
         name: 'Description',
-        message: 'Give a description of your project.',
+        message: 'Give a description of your project. (N/A will ',
+        default: "N/A",
     },
     {
         type: 'input',
         name: 'Usage',
-        message: 'How will your project be used?',
+        message: 'How will your project be used? (N/A will be inserted of no usage is input.)',
+        default: "N/A",
     },
     {
         type: 'input',
         name: 'Installation',
-        message: 'What steps are required for installing your project. (N/A if no installation is required.)',
+        message: 'What steps are required for installing your project. (If no installation is required, N/A will be placed.)',
+        default: "N/A",
+
     },
     {
         type: 'input',
         name: 'Contributers',
         message: 'List any contributers to the project using GitHub user names.',
+        default: "",
     },
     {
-        type: 'list',
+        type: 'checkbox',
         name: 'License',
         message: 'What license does your project have?',
+        choices: ["MIT","APACHE 2.0","GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "MPL 2.0", "Boost 1.0", "Unlicense"],
+    },
+    {
+        type: 'input',
+        name: 'Tests',
+        message: 'Please explain what tests were done.',
+        default: "N/A",
     }
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    return fs.writeFileSync()
-}
+function write () {
+    fs.writeFile('README.md', fileRM, (err) => err ? console.log(err) :  console.log("Generating README..."));
+};
 
-// TODO: Create a function to initialize app
+
 function init() {
-    
-}
-
+    inquirer.prompt(questions).then((answers) => {
+        const fileRM = generateMarkdown(answers);
+        write();
+       
+});
+};
 // Function call to initialize app
 init();
